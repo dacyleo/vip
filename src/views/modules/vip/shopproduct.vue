f54f99567ed6fbbf29d4fbda9e5155e0885d75e4
cloud <136060410@qq.com>
2020-09-12 12:56:24 +0800

    上下架及查询

--- src/views/modules/vip/shopproduct.vue
+++ src/views/modules/vip/shopproduct.vue
@@ -3,16 +3,23 @@
     <div class="mod-vip__shopproduct}">
       <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
         <el-form-item>
-          <el-input v-model="dataForm.id" placeholder="id" clearable></el-input>
+          <el-input v-model="dataForm.shopName" placeholder="商户名称" clearable></el-input>
         </el-form-item>
         <el-form-item>
           <el-button @click="getDataList()">{{ $t('query') }}</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-button type="info" @click="exportHandle()">{{ $t('export') }}</el-button>
-        </el-form-item>
+<!--        <el-form-item>-->
+<!--          <el-button type="info" @click="exportHandle()">{{ $t('export') }}</el-button>-->
+<!--        </el-form-item>-->
         <el-form-item>
           <el-button v-if="$hasPermission('vip:shopproduct:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button v-if="$hasPermission('vip:shopproduct:save')" type="warning" @click="onHandle()">上架</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button v-if="$hasPermission('vip:shopproduct:save')" type="warning" @click="offHandle()">下架
+          </el-button>
         </el-form-item>
         <el-form-item>
           <el-button v-if="$hasPermission('vip:shopproduct:delete')" type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
@@ -20,16 +27,20 @@
       </el-form>
       <el-table v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" style="width: 100%;">
         <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-        <el-table-column prop="id" label="ID" header-align="center" align="center"></el-table-column>
+<!--        <el-table-column prop="id" label="ID" header-align="center" align="center"></el-table-column>-->
+        <el-table-column prop="shopName" label="所属商户" header-align="center" align="center"></el-table-column>
         <el-table-column prop="price" label="优惠券金额" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="shopId" label="商户ID" header-align="center" align="center"></el-table-column>
         <el-table-column prop="title" label="优惠券简介" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="content" label="优惠券说明" header-align="center" align="center"></el-table-column>
+<!--        <el-table-column prop="content" label="优惠券说明" header-align="center" align="center"></el-table-column>-->
         <el-table-column prop="drawAmount" label="领取个数" header-align="center" align="center"></el-table-column>
         <el-table-column prop="verifyAmount" label="核销个数" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="createTime" label="" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="state" label="0草稿1上架2下架" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="picUrl" label="商品图片" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="state" label="状态" :formatter="figureState" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="picUrl" label="商品图片" header-align="center" align="center">
+          <template slot-scope="scope">
+            <img :src="scope.row.picUrl" width="100" height="60" class="head_pic"/>
+          </template>
+        </el-table-column>
         <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
           <template slot-scope="scope">
             <el-button v-if="$hasPermission('vip:shopproduct:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('update') }}</el-button>
@@ -64,6 +75,8 @@ export default {
         getDataListIsPage: true,
         exportURL: '/vip/shopproduct/export',
         deleteURL: '/vip/shopproduct',
+        onURL: '/vip/shopproduct/on',
+        offURL: '/vip/shopproduct/off',
         deleteIsBatch: true
       },
       dataForm: {
@@ -73,6 +86,81 @@ export default {
   },
   components: {
     AddOrUpdate
+  },
+  methods: {
+    figureState (row, column) {
+      if (row.state == null) {
+        return ''
+      }
+      if (row.state === 0) {
+        return '草稿'
+      } else if (row.state === 1) {
+        return '已上架'
+      } else if (row.state === 2) {
+        return '已下架'
+      } else {
+        return '未知'
+      }
+    },
+    //上架
+    onHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length == 0) {
+        return this.$message({
+          message: '请选择上架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行上架操作?', { 'handle': '上架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.onURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
+    offHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length === 0) {
+        return this.$message({
+          message: '请选择下架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行下架操作?', { 'handle': '下架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.offURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
   }
 }
 </script>
