7805293d00bbbbc585aefdea74dcb2f9d8be9158
cloud <136060410@qq.com>
2020-09-12 15:33:52 +0800

    商户banner功能开发

--- src/views/modules/vip/shopbanner-add-or-update.vue
+++ src/views/modules/vip/shopbanner-add-or-update.vue
@@ -1,117 +1,164 @@
 <template>
-  <el-dialog :visible.sync="visible" :title="!dataForm.id ? $t('add') : $t('update')" :close-on-click-modal="false" :close-on-press-escape="false">
-    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmitHandle()" :label-width="$i18n.locale === 'en-US' ? '120px' : '80px'">
-          <el-form-item label="所属商户ID" prop="shopId">
-          <el-input v-model="dataForm.shopId" placeholder="所属商户ID"></el-input>
-      </el-form-item>
-          <el-form-item label="banner标题" prop="title">
-          <el-input v-model="dataForm.title" placeholder="banner标题"></el-input>
-      </el-form-item>
-          <el-form-item label="banner图片" prop="imageUrl">
-          <el-input v-model="dataForm.imageUrl" placeholder="banner图片"></el-input>
-      </el-form-item>
-          <el-form-item label="banner链接URL" prop="refUrl">
-          <el-input v-model="dataForm.refUrl" placeholder="banner链接URL"></el-input>
-      </el-form-item>
-          <el-form-item label="0草稿1上架2下架" prop="state">
-          <el-input v-model="dataForm.state" placeholder="0草稿1上架2下架"></el-input>
-      </el-form-item>
-          <el-form-item label="创建时间" prop="createTime">
-          <el-input v-model="dataForm.createTime" placeholder="创建时间"></el-input>
-      </el-form-item>
-      </el-form>
-    <template slot="footer">
-      <el-button @click="visible = false">{{ $t('cancel') }}</el-button>
-      <el-button type="primary" @click="dataFormSubmitHandle()">{{ $t('confirm') }}</el-button>
-    </template>
-  </el-dialog>
+    <el-dialog :visible.sync="visible" :title="!dataForm.id ? $t('add') : $t('update')" :close-on-click-modal="false"
+               :close-on-press-escape="false">
+        <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmitHandle()"
+                 :label-width="$i18n.locale === 'en-US' ? '120px' : '80px'">
+            <el-form-item label="所属商户" prop="shopId">
+                <el-select v-model="dataForm.shopId" placeholder="请选择商户">
+                    <el-option v-for="(item,index) in shopList" :key="index" :label="item.name"
+                               :value="item.id"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="banner标题" prop="title">
+                <el-input v-model="dataForm.title" placeholder="banner标题"></el-input>
+            </el-form-item>
+            <el-form-item label="banner图片" prop="imageUrl">
+                <el-upload
+                        class="avatar-uploader"
+                        action=""
+                        :http-request="uploadFileElment"
+                        :show-file-list="false"
+                        :on-success="handleSuccess"
+                        :before-upload="beforeUpload">
+                    <img style="width: 94px;height: 94px;"
+                         v-if="dataForm.imageUrl" :src="dataForm.imageUrl">
+                    <img style="width: 94px;height: 94px;" v-else
+                         src="../../../assets/img/update.png">
+                </el-upload>
+            </el-form-item>
+            <el-form-item label="banner链接URL" prop="refUrl">
+                <el-input v-model="dataForm.refUrl" placeholder="banner链接URL"></el-input>
+            </el-form-item>
+        </el-form>
+        <template slot="footer">
+            <el-button @click="visible = false">{{ $t('cancel') }}</el-button>
+            <el-button type="primary" @click="dataFormSubmitHandle()">{{ $t('confirm') }}</el-button>
+        </template>
+    </el-dialog>
 </template>
 
 <script>
-import debounce from 'lodash/debounce'
-export default {
-  data () {
-    return {
-      visible: false,
-      dataForm: {
-        id: '',
-        shopId: '',
-        title: '',
-        imageUrl: '',
-        refUrl: '',
-        state: '',
-        createTime: ''
-      }
-    }
-  },
-  computed: {
-    dataRule () {
-      return {
-        shopId: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ],
-        title: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ],
-        imageUrl: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ],
-        refUrl: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ],
-        state: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ],
-        createTime: [
-          { required: true, message: this.$t('validate.required'), trigger: 'blur' }
-        ]
-      }
-    }
-  },
-  methods: {
-    init () {
-      this.visible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].resetFields()
-        if (this.dataForm.id) {
-          this.getInfo()
-        }
-      })
-    },
-    // 获取信息
-    getInfo () {
-      this.$http.get(`/vip/shopbanner/${this.dataForm.id}`).then(({ data: res }) => {
-        if (res.code !== 0) {
-          return this.$message.error(res.msg)
-        }
-        this.dataForm = {
-          ...this.dataForm,
-          ...res.data
-        }
-      }).catch(() => {})
-    },
-    // 表单提交
-    dataFormSubmitHandle: debounce(function () {
-      this.$refs['dataForm'].validate((valid) => {
-        if (!valid) {
-          return false
-        }
-        this.$http[!this.dataForm.id ? 'post' : 'put']('/vip/shopbanner/', this.dataForm).then(({ data: res }) => {
-          if (res.code !== 0) {
-            return this.$message.error(res.msg)
-          }
-          this.$message({
-            message: this.$t('prompt.success'),
-            type: 'success',
-            duration: 500,
-            onClose: () => {
-              this.visible = false
-              this.$emit('refreshDataList')
+    import debounce from 'lodash/debounce'
+    import Cookies from 'js-cookie'
+
+    export default {
+        data() {
+            return {
+                visible: false,
+                dataForm: {
+                    id: '',
+                    shopId: '',
+                    title: '',
+                    imageUrl: '',
+                    refUrl: '',
+                    state: '',
+                    createTime: ''
+                },
+                shopList: []
             }
-          })
-        }).catch(() => {})
-      })
-    }, 1000, { 'leading': true, 'trailing': false })
-  }
-}
+        },
+        computed: {
+            dataRule() {
+                return {
+                    shopId: [
+                        {required: true, message: this.$t('validate.required'), trigger: 'blur'}
+                    ],
+                    title: [
+                        {required: true, message: this.$t('validate.required'), trigger: 'blur'}
+                    ],
+                    imageUrl: [
+                        {required: true, message: this.$t('validate.required'), trigger: 'blur'}
+                    ]
+                }
+            }
+        },
+        methods: {
+            init() {
+                this.visible = true
+                this.getShop()
+                this.$nextTick(() => {
+                    this.$refs['dataForm'].resetFields()
+                    if (this.dataForm.id) {
+                        this.getInfo()
+                    }
+                })
+            },
+            getShop() {
+                this.$http.get(`/vip/shop/list`).then(({data: res}) => {
+                    if (res.code !== 0) {
+                        return this.$message.error(res.msg)
+                    }
+                    this.shopList = {
+                        ...this.shopList,
+                        ...res.data
+                    }
+                }).catch(() => {
+                })
+            },
+            // 获取信息
+            getInfo() {
+                this.$http.get(`/vip/shopbanner/${this.dataForm.id}`).then(({data: res}) => {
+                    if (res.code !== 0) {
+                        return this.$message.error(res.msg)
+                    }
+                    this.dataForm = {
+                        ...this.dataForm,
+                        ...res.data
+                    }
+                }).catch(() => {
+                })
+            },
+            handleSuccess: function (res, file) {
+                this.dataForm.imageUrl = res.data.data.src
+            },
+            beforeUpload: function (file) {
+                var isJPG = (file.type === 'image/jpeg' || file.type === 'image/png')
+                var isLt2M = file.size / 1024 / 1024 < 2
+
+                if (!isJPG) {
+                    this.$message.error("上传图片格式只能是JPG/JPEG/PNG/三种格式！")
+                }
+                if (!isLt2M) {
+                    this.$message.error("上传图片大小不能超过 2MB!")
+                }
+                return isJPG && isLt2M
+            },
+            uploadFileElment: function (content) {
+                var form = new FormData()
+                form.append('name', content.file.name)
+                form.append('file', content.file)
+                this.$http.post("/sys/oss/upload?token=" + Cookies.get('token'), form, {
+                    timeout: 0,
+                    headers: {"Content-Type": "multipart/form-data"}
+                }).then(function (res) {
+                    content.onSuccess(res)
+                }, function (error) {
+                    content.onError(error)
+                })
+            },
+            // 表单提交
+            dataFormSubmitHandle: debounce(function () {
+                this.$refs['dataForm'].validate((valid) => {
+                    if (!valid) {
+                        return false
+                    }
+                    this.$http[!this.dataForm.id ? 'post' : 'put']('/vip/shopbanner/', this.dataForm).then(({data: res}) => {
+                        if (res.code !== 0) {
+                            return this.$message.error(res.msg)
+                        }
+                        this.$message({
+                            message: this.$t('prompt.success'),
+                            type: 'success',
+                            duration: 500,
+                            onClose: () => {
+                                this.visible = false
+                                this.$emit('refreshDataList')
+                            }
+                        })
+                    }).catch(() => {
+                    })
+                })
+            }, 1000, {'leading': true, 'trailing': false})
+        }
+    }
 </script>
--- src/views/modules/vip/shopbanner.vue
+++ src/views/modules/vip/shopbanner.vue
@@ -3,13 +3,20 @@
     <div class="mod-vip__shopbanner}">
       <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
         <el-form-item>
-          <el-input v-model="dataForm.id" placeholder="id" clearable></el-input>
+          <el-input v-model="dataForm.shopName" placeholder="所属商户" clearable></el-input>
         </el-form-item>
         <el-form-item>
           <el-button @click="getDataList()">{{ $t('query') }}</el-button>
         </el-form-item>
+<!--        <el-form-item>-->
+<!--          <el-button type="info" @click="exportHandle()">{{ $t('export') }}</el-button>-->
+<!--        </el-form-item>-->
         <el-form-item>
-          <el-button type="info" @click="exportHandle()">{{ $t('export') }}</el-button>
+          <el-button v-if="$hasPermission('vip:shopproduct:save')" type="warning" @click="onHandle()">上架</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button v-if="$hasPermission('vip:shopproduct:save')" type="warning" @click="offHandle()">下架
+          </el-button>
         </el-form-item>
         <el-form-item>
           <el-button v-if="$hasPermission('vip:shopbanner:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
@@ -20,12 +27,16 @@
       </el-form>
       <el-table v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" style="width: 100%;">
         <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-        <el-table-column prop="id" label="ID" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="shopId" label="所属商户ID" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="title" label="banner标题" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="imageUrl" label="banner图片" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="refUrl" label="banner链接URL" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="state" label="0草稿1上架2下架" header-align="center" align="center"></el-table-column>
+<!--        <el-table-column prop="id" label="ID" header-align="center" align="center"></el-table-column>-->
+        <el-table-column prop="shopName" label="所属商户" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="title" label="标题" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="imageUrl" label="图片" header-align="center" align="center">
+          <template slot-scope="scope">
+            <img :src="scope.row.imageUrl" width="100" height="60" class="head_pic"/>
+          </template>
+        </el-table-column>
+<!--        <el-table-column prop="refUrl" label="banner链接URL" header-align="center" align="center"></el-table-column>-->
+        <el-table-column prop="state" label="状态" :formatter="figureState" header-align="center" align="center"></el-table-column>
         <el-table-column prop="createTime" label="创建时间" header-align="center" align="center"></el-table-column>
         <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
           <template slot-scope="scope">
@@ -61,6 +72,8 @@ export default {
         getDataListIsPage: true,
         exportURL: '/vip/shopbanner/export',
         deleteURL: '/vip/shopbanner',
+        onURL: '/vip/shopbanner/on',
+        offURL: '/vip/shopbanner/off',
         deleteIsBatch: true
       },
       dataForm: {
@@ -70,6 +83,140 @@ export default {
   },
   components: {
     AddOrUpdate
+  },
+  methods: {
+    //上架
+    onHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length == 0) {
+        return this.$message({
+          message: '请选择上架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行上架操作?', { 'handle': '上架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.onURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
+    offHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length === 0) {
+        return this.$message({
+          message: '请选择下架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行下架操作?', { 'handle': '下架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.offURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
+    figureState (row, column) {
+      if (row.state == null) {
+        return ''
+      }
+      if (row.state === 0) {
+        return '草稿'
+      } else if (row.state === 1) {
+        return '已上架'
+      } else if (row.state === 2) {
+        return '已下架'
+      } else {
+        return '未知'
+      }
+    },
+    //上架
+    onHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length == 0) {
+        return this.$message({
+          message: '请选择上架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行上架操作?', { 'handle': '上架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.onURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
+    offHandle () {
+      if (!this.dataListSelections || this.dataListSelections.length === 0) {
+        return this.$message({
+          message: '请选择下架项',
+          type: 'warning',
+          duration: 1000
+        })
+      }
+      this.$confirm('确定进行下架操作?', { 'handle': '下架' }, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.post(`${this.mixinViewModuleOptions.offURL}`, this.dataListSelections.map(item => item['id'])
+        ).then(({ data: res }) => {
+          if (res.code !== 0) {
+            return this.$message.error(res.msg)
+          }
+          this.$message({
+            message: this.$t('prompt.success'),
+            type: 'success',
+            duration: 1000,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        })
+      })
+    },
   }
 }
 </script>
